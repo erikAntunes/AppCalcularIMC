236e1af7e04ad12651462afdcf417394587005cb
unknown <Digital@DH-A7-03.dhbrasil.digitalhouse.com>
2019-05-24 12:01:25 -0300

    Primeiro Commit

---
+++ app/src/main/java/br/com/digitalhouse/myapplication/MainActivity.java
@@ -0,0 +1,37 @@
+package br.com.digitalhouse.myapplication;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.TextView;
+import android.widget.Toast;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+    public void botaoApertado(View view){
+
+        System.out.println("Botão foi apertado");
+        TextView respostaTextView = findViewById(R.id.text_resposta_id);
+        EditText pesoDigitado = findViewById(R.id.peso_digitado_id);
+        EditText alturaDigitada = findViewById(R.id.altura_digitada_id);
+
+        Float peso = Float.parseFloat(pesoDigitado.getText().toString());
+        Float altura = Float.parseFloat(alturaDigitada.getText().toString());
+
+        Float resultado = peso / (altura * altura);
+
+
+
+        respostaTextView.setText("Seu IMC é: "+resultado);
+
+        Toast.makeText(this,"Calculado com Sucesso",Toast.LENGTH_LONG).show();
+
+    }
+}
